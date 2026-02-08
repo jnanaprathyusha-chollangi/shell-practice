266ab239ef111d7c5b653c3a0703c6ae274bfc33
prathyusha <chollangiprathyusha@gmail.com>
2026-02-08 14:35:13 +0530

    done

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Ramesh:: Hello suresh...! how ae you"
-eho "Suresh:: hi Ramesh .. i am fine what about you"
+echo "Suresh:: hi Ramesh .. i am fine what about you"
 echo "Ramesh:: i am aboustely fine, great to meet to ramesh"
