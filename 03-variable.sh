f9cc056c2dfb9cd6ebe23e7af9c23c1a504fda31
prathyusha <chollangiprathyusha@gmail.com>
2026-02-08 15:54:03 +0530

    variable refer

---
+++ 03-variable.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+PERSON1=Ramesh # no space b/w the value and =
+PERSON2=Suresh
+
+echo "$PERSON1:: Hello $PERSON2...! how ae you"
+echo "$PERSON2:: hi $PERSON1 .. i am fine what about you"
+echo "$PERSON1:: i am aboustely fine, great to meet to $PERSON2"
