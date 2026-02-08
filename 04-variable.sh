9927c0a69e6172e48af764b70a00745f814a4eb7
prathyusha <chollangiprathyusha@gmail.com>
2026-02-08 16:16:52 +0530

    changes are completed

---
+++ 04-variable.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+PERSON1=$1 # no space b/w the value and =
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2...! how ae you"
+echo "$PERSON2:: hi $PERSON1 .. i am fine what about you"
+echo "$PERSON1:: i am aboustely fine, great to meet to $PERSON2"
