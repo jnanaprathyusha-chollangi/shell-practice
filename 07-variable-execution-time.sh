9ac80422eaa998a0606f3abfa5688871b4ec0f9a
prathyusha <chollangiprathyusha@gmail.com>
2026-02-09 20:44:31 +0530

    date execution

---
+++ 07-variable-execution-time.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+START_TIME=$(date +%s)
+
+echo "Script executed at : $START_TIME"
+sleep 10 
+END_TIME=$(date +%s)
+TOTAL_TIME = $(($END_TIME-$START_TIME)) 
+echo "Total script executed time in:$TOTAL_TIME"
