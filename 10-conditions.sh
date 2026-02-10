dbba97f548acd3172895f48cc9dbfcf22e45b8c1
prathyusha <chollangiprathyusha@gmail.com>
2026-02-10 17:03:08 +0530

    changes are done

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 NUM=779
-if [ $NUM -gt 779];then
+if [ $NUM -gt 779 ];then
     echo "given number is greater than: $NUM"
-elif [ $NUM -lt 779];then
+elif [ $NUM -lt 779 ];then
     echo "given number is less then: $NUM"
 else 
     echo "given number is equal to : $NUM"
