7ade3b259104907f37cf24269805e366ab133312
prathyusha <chollangiprathyusha@gmail.com>
2026-02-10 16:50:02 +0530

    changes are done

---
+++ 10-conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NUM=$1
+if [ $NUM -gt 20] ; then
+    echo "given number is greater than: $NUM"
+elif [ $NUM -lt 20] ; then
+    echo "given number is less then: $NUM"
+else 
+    echo "given number is equal to : $NUM"
+fi
