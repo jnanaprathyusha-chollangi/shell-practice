09e51435781f123e4750c8e613c840c1af1a3872
prathyusha <chollangiprathyusha@gmail.com>
2026-02-10 17:33:38 +0530

    changes are done

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 USER_ID=$(id -u)
 
-if [ USER_ID -ne 0 ]; then
+if [ $USER_ID -ne 0 ];then
     echo "please the run the script with root user"
     exit 1
 fi 
-echo "Installing NGINX"
+echo "Installing nginx"
 dnf install nginx -y
 
 if [ $? -ne 0 ]; then 
     echo "Installing the nginx ..FALIURE"
+    exit 1
 elif 
     echo "Installing the nginx...sucess"
 fi
