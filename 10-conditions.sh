0073be40bc0f0077fad1f2c4ac554059ca6e99f7
prathyusha <chollangiprathyusha@gmail.com>
2026-02-10 16:36:09 +0530

    changes are done

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,17 +1,4 @@
-#!bin/bash
-# NUMBER=$779
-# # -gt -> gerather than
-# # -lt -> less than
-# # -eq -> equal
-# #-ne -> not equal
-# if [$NUMBER -gt 1000]; then
-#     echo "given number is greater than:$NUMBER"
-# elif [$NUMBER -lt 1000] ; then 
-#     echo "given number is less than:$NUMBER"
-# fi
-
-
-
+#!/bin/bash
 NUMBER=$1
 
 # -gt -> greater than
