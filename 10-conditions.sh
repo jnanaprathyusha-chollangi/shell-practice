4bb70103f4f4526b9336483bb55f83dc6fc0d5af
prathyusha <chollangiprathyusha@gmail.com>
2026-02-10 16:28:22 +0530

    changes are done

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -5,11 +5,7 @@ NUMBER=$779
 # -eq -> equal
 #-ne -> not equal
 if [$NUMBER -gt 1000]; then
- echo "given is greather than:$NUMBER"
+    echo "given number is greater than:$NUMBER"
 elif [$NUMBER -lt 1000] ; then 
- echo "given number is less than:$NUMBER"
-elif [$NUMBER -eq 1000] ; then 
- echo "given number equal to :$NUMBER"
-else 
- echo "given number is not equal:$NUMBER"
+    echo "given number is less than:$NUMBER"
 fi
