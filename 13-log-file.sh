f17ce40c3c8ff5d7a0eeade40774ea40aca44534
prathyusha <chollangiprathyusha@gmail.com>
2026-02-11 22:50:12 +0530

    changes are done

--- 13-log-file.sh
+++ 13-log-file.sh
@@ -4,15 +4,15 @@ USER_ID=$(id -u)
 LOG_FLODER="/var/log/shell-script"
 LOG_FILE="/var/log/shell-script/$0.log"
 if [ $USER_ID -ne 0 ] ; then
-    echo "please run this script with root user access"
+    echo "please run this script with root user access" | tee -a $LOG_FILE
     exit 1 
 fi 
 VALIDATE(){
     if [ $1 -ne 0 ]; then 
-        echo "$2...FALIURE"
+        echo "$2...FALIURE" | tee -a $LOG_FILE
         exit 1 
     else 
-        echo "$2...SUCCESS"
+        echo "$2...SUCCESS" | tee -a $LOG_FILE
     fi
 }
 dnf install nginx -y &>> $LOG_FILE
