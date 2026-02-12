426784af7f51b41728cd15de9e84dde018141ccb
prathyusha <chollangiprathyusha@gmail.com>
2026-02-12 11:52:30 +0530

    changes are done

---
+++ 16-loops.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+LOG_FLODER="/var/log/shell-script"
+LOG_FILE="/var/log/shell-script/$0.log"
+
+if [ $USER_ID -ne 0 ] ; then
+    echo "please run this script with root user access" | tee -a $LOG_FILE
+    exit 1 
+fi 
+mkdir -p $LOG_FLODER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then 
+        echo "$2...FALIURE" | tee -a $LOG_FILE
+        exit 1 
+    else 
+        echo "$2...SUCCESS" | tee -a $LOG_FILE
+    fi
+}
+for package in $@ #sudo sh 15-loop.sh nginx mysql nodejs
+do 
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ]; then
+        echo "$packed not installed, installing now"
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "$package Installing"
+    else
+        echo"$package already installed,skipping"
+    if
+done
