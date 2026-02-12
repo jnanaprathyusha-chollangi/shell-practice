f3b9def07a4bb886d2136fcfa1d593269e88c85a
prathyusha <chollangiprathyusha@gmail.com>
2026-02-12 11:24:20 +0530

    changes are done

--- 15-loops.sh
+++ 15-loops.sh
@@ -5,17 +5,17 @@ LOG_FLODER="/var/log/shell-script"
 LOG_FILE="/var/log/shell-script/$0.log"
 
 if [ $USER_ID -ne 0 ] ; then
-    echo "please run this script with root user access" &>>$LOG_FILE
+    echo "please run this script with root user access" | tee -a $LOG_FILE
     exit 1 
 fi 
 mkdir -p $LOG_FLODER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then 
-        echo "$2...FALIURE" &>>$LOG_FILE
+        echo "$2...FALIURE" | tee -a $LOG_FILE
         exit 1 
     else 
-        echo "$2...SUCCESS" &>>$LOG_FILE
+        echo "$2...SUCCESS" | tee -a $LOG_FILE
     fi
 }
 for package in $@ #sudo sh 15-loop.sh nginx mysql nodejs
