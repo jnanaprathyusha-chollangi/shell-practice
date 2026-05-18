806e9877a4fdd4511c296bb1759a7009514f1f24
prathyusha <chollangiprathyusha@gmail.com>
2026-05-18 19:31:36 +0530

    confiure backup code

--- 25-backup.sh
+++ 25-backup.sh
@@ -3,7 +3,9 @@
 USERID=$(id -u)
 LOGS_DIR="/var/log/shell-script"
 LOGS_FILE="$LOGS_DIR/backup.log"
-
+SOURCE_DIR=
+DEST_DIR=
+DAYS=${3:-14} # 14 days is the default value, if the user not give 
 
 R="\e[31m"
 G="\e[32m"
@@ -26,40 +28,12 @@ if [ $# -lt 2 ]; then
     USAGE
 fi
 
+if [ -d $SOURCE_DIR ]; then 
+    echo "$R source_DIR does not exit $N"
+    exit 1
+fi 
 
-
-
-# #!/bin/bash
-
-# USERID=$(id -u)
-# LOGS_FOLDER="/var/log/shell-script"
-# LOGS_FILE="/var/log/shell-script/backup.log"
-# R="\e[31m"
-# G="\e[32m"
-# Y="\e[33m"
-# N="\e[0m"
-# # SOURCE_DIR=$1
-# # DEST_DIR=$2
-# # DAYS=${3:-14} # 14 days is the default value, if the user not supplied
-
-# log(){
-#     echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
-# }
-
-# if [ $USERID -ne 0 ]; then
-#     echo -e "$R Please run this script with root user access $N"
-#     exit 1
-# fi
-
-# mkdir -p $LOGS_FOLDER
-
-# USAGE(){
-#     log "$R USAGE:: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS>[default 14 days] $N"
-#     exit 1
-# }
-
-
-# if [ $# -lt 2 ]; then
-#     USAGE
-# fi
-
+if [ -d $DEST_DIR ]; then 
+    echo "$R DEST_DIR does not exit $N"
+    exit 1 
+fi 
