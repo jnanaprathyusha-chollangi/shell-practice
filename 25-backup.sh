518fd271d094b59f2f8ac365dea37d4c4bbba004
prathyusha <chollangiprathyusha@gmail.com>
2026-05-18 20:12:16 +0530

    confiure backup code

--- 25-backup.sh
+++ 25-backup.sh
@@ -24,6 +24,13 @@ USAGE() {
     exit 1
 }
 
+log(){
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
+}
+
+
+
+
 if [ $# -lt 2 ]; then
     USAGE
 fi
@@ -37,4 +44,13 @@ if [ -d $DEST_DIR ]; then
     echo -e "$R $DEST_DIR does not exit $N"
     exit 1 
 fi 
+
+### find the files
+
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+
+log "Backup started"
+log "Source Directory: $SOURCE_DIR"
+log "Destination Directory: $DEST_DIR"
+log "Days: $DAYS"
  
