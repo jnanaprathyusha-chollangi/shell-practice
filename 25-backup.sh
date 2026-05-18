8bb4482944ca4ca834b7a5c1e89e8025b40abedb
prathyusha <chollangiprathyusha@gmail.com>
2026-05-18 12:57:53 +0530

    delete 14 days old logs

--- 25-backup.sh
+++ 25-backup.sh
@@ -9,18 +9,17 @@ N="\e[34m"
 
 mkdir -p $LOGS_DIR
 
-if [ $USERID -ne 0 ]; then 
-    echo -e "$R please run the script with user access $N" | tee -a $LOGS_FILE
-    exit 1 
-fi 
-
-USAGE(){
-    echp -e "$R USAGE:: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS>[default 14 days] $N"
-    exit 1 
-}
-
-if [ $# -lt 2 ]; then 
-    USAGE
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run the script with root access $N" | tee -a $LOGS_FILE
+    exit 1
 fi
 
+USAGE() {
+    echo -e "$R USAGE:: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS>[default 14 days] $N"
+    exit 1
+}
+
+if [ $# -lt 2 ]; then
+    USAGE
+fi
 
