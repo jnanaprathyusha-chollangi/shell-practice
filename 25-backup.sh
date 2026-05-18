a0c9cc7ba5c524be5ad3f61af244631ec4df62a4
prathyusha <chollangiprathyusha@gmail.com>
2026-05-18 16:46:12 +0530

    confiure backup code

--- 25-backup.sh
+++ 25-backup.sh
@@ -1,23 +1,60 @@
-USERID=$(id -u)
-LOGS_FILE=$LOGS_DIR/backup.log"
-LOGS_DIR="/var/log/shell-script"
+# USERID=$(id -u)
+# LOGS_FILE=$LOGS_DIR/backup.log"
+# LOGS_DIR="/var/log/shell-script"
 
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+
+# mkdir -p $LOGS_DIR
+
+# if [ $USERID -ne 0 ]; then
+#     echo -e "$R Please run the script with root access $N" | tee -a $LOGS_FILE
+#     exit 1
+# fi
+
+# USAGE() {
+#     echo -e "$R USAGE:: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS>[default 14 days] $N"
+#     exit 1
+# }
+
+# if [ $# -lt 2 ]; then
+#     USAGE
+# fi
+
+
+
+
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/backup.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+# SOURCE_DIR=$1
+# DEST_DIR=$2
+# DAYS=${3:-14} # 14 days is the default value, if the user not supplied
 
-mkdir -p $LOGS_DIR
+log(){
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
+}
 
 if [ $USERID -ne 0 ]; then
-    echo -e "$R Please run the script with root access $N" | tee -a $LOGS_FILE
+    echo -e "$R Please run this script with root user access $N"
     exit 1
 fi
 
-USAGE() {
-    echo -e "$R USAGE:: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS>[default 14 days] $N"
+mkdir -p $LOGS_FOLDER
+
+USAGE(){
+    log "$R USAGE:: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS>[default 14 days] $N"
     exit 1
 }
+
 
 if [ $# -lt 2 ]; then
     USAGE
